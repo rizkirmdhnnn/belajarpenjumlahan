ecc41b7e1845bcaf2228f8b3107e6bc02f326a3d
rizpedia <achmadrizkiramadhan0101@gmail.com>
2022-07-23 14:13:06 +0700

    fix soal ga muncul pas awan

--- assets/datalvl.js
+++ assets/datalvl.js
@@ -1,30 +1,30 @@
-const dataLvl = 'dataLvl';
-
-function lvlMudah(){
-    alert('Level mudah')
-    sessionStorage.setItem(dataLvl, 'mudah');
-    window.location.assign(window.location.protocol);
-}
-
-function lvlSedang(){
-    alert('Level sedang')
-    sessionStorage.setItem(dataLvl, 'sedang');
-    window.location.assign(window.location.protocol);
-}
-
-function lvlSusah(){
-    alert('Level susah')
-    sessionStorage.setItem(dataLvl, 'susah');
-    window.location.assign(window.location.protocol);
-}
+const dataLvl = 'dataLvl'
 
 // Cek browser support localStorage tidak
 if (typeof(Storage) !== 'undefined') {
     // Browser mendukung sessionStorage/localStorage.
-    if(sessionStorage.getItem(dataLvl) == 'undefined'){
-        alert('data blm ada')
+    if(typeof(sessionStorage.getItem(dataLvl)) === 'object'){
+        sessionStorage.setItem(dataLvl, 'mudah');
+    }
+    function lvlMudah(){
+        alert('Level mudah')
+        sessionStorage.setItem(dataLvl, 'mudah');
+        window.location.assign(window.location.protocol);
+    }
+    
+    function lvlSedang(){
+        alert('Level sedang')
+        sessionStorage.setItem(dataLvl, 'sedang');
+        window.location.assign(window.location.protocol);
+    }
+    
+    function lvlSusah(){
+        alert('Level susah')
+        sessionStorage.setItem(dataLvl, 'susah');
+        window.location.assign(window.location.protocol);
     }
 } else {
     // Browser tidak mendukung sessionStorage/LocalStorage
+    alert('Browsermu ga support session storage bro')
 
 }
